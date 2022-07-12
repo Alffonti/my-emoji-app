e5656bce9ffc2777971c0c48a29fadac69a0f600
Alffonti <alffonti@gmail.com>
2022-07-12 20:06:35 -0300

    Added Emojis
    Add a watermelon and a surfing emoji

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["🧑🏽‍💻", "🍲", "🏃🏽", "✈️"]
+const myEmojis = ["🧑🏽‍💻", "🍉", "🛌", "🏄"]
 const emojiContainer = document.getElementById("emoji-container")
 const emojiInput = document.getElementById('emoji-input')
 const pushBtn = document.getElementById('push-btn')
