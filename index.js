b27884d3bb91eeda88663c12c0b5f4c929cc5d89
Alffonti <alffonti@gmail.com>
2022-07-12 19:31:15 -0300

    Initialized Project Files
    Initializing a repo with Github Desktop

---
+++ index.js
@@ -0,0 +1,44 @@
+const myEmojis = ["🧑🏽‍💻", "🍲", "🏃🏽"]
+const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById('emoji-input')
+const pushBtn = document.getElementById('push-btn')
+const unshiftBtn = document.getElementById('unshift-btn')
+const popBtn = document.getElementById('pop-btn')
+const shiftBtn = document.getElementById('shift-btn')
+
+function renderEmojis() {
+    emojiContainer.innerHTML = '' // clear the `emojiContainer` HTML element
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span') // create a HTML element
+        emoji.textContent = myEmojis[i] // add content to the `emoji` HTML element
+        emojiContainer.append(emoji) // insert the the `emoji` HTML element to the last child of the `emojiContainer` HTML element
+    }
+}
+
+renderEmojis()
+
+pushBtn.addEventListener('click', function() {
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = '' // clear the `emojiInput` value
+        renderEmojis()
+    }
+})
+
+unshiftBtn.addEventListener('click', function() {
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = '' // clear the `emojiInput` value
+        renderEmojis()
+    }
+})
+
+popBtn.addEventListener('click', function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener('click', function() {
+    myEmojis.shift()
+    renderEmojis()
+})
